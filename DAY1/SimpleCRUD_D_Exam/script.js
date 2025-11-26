94cee054a3eabbdc1957f15c9ad68fb981dca3ff
namrapithwa-tech <164359724+namrapithwa-tech@users.noreply.github.com>
2025-11-26 11:10:41 +0530

    DAY02 Local Storage Implemented

--- DAY1/SimpleCRUD_D_Exam/script.js
+++ DAY1/SimpleCRUD_D_Exam/script.js
@@ -1,32 +1,4 @@
-const facultyData = [];
-
-function addFaculty(){
-    let facName = document.getElementById("facultyName").value;
-    let facDept = document.getElementById("facultyDepartment").value;
-    let facEmail = document.getElementById("facultyEmail").value;
-    let facPhone = document.getElementById("facultyPhone").value;
-    let facDesigntion = document.getElementById("facultyDesignation").value;
-
-    let faculty = {
-        fac_name: facName,
-        fac_department: facDept,
-        fac_email: facEmail,
-        fac_phone: facPhone,
-        fac_designation: facDesigntion
-    };
-
-    alert("Faculty Added Successfully..!!");
-
-    console.log("Faculty Object :");
-    console.log(faculty);
-
-    facultyData.push(faculty);
-    console.log("Faculty in Array :");
-    console.log(facultyData);
-    
-    displayFaculty_In_Table();
-}
-
+const facultyData = JSON.parse(localStorage.getItem("point")) || [];
 function displayFaculty_In_Table(){
     document.getElementById("facultyTableBody").innerHTML =
     facultyData.map((faculty, index) => `
@@ -44,6 +16,37 @@ function displayFaculty_In_Table(){
 function deleteFaculty(index){
     facultyData.splice(index, 1);
     alert("Faculty Deleted Successfully..!!");
+    localStorage.setItem("point", JSON.stringify(facultyData));
     displayFaculty_In_Table();
 }
 
+function addFaculty(){
+    let facName = document.getElementById("facultyName").value;
+    let facDept = document.getElementById("facultyDepartment").value;
+    let facEmail = document.getElementById("facultyEmail").value;
+    let facPhone = document.getElementById("facultyPhone").value;
+    let facDesigntion = document.getElementById("facultyDesignation").value;
+
+    let faculty = {
+        fac_name: facName,
+        fac_department: facDept,
+        fac_email: facEmail,
+        fac_phone: facPhone,
+        fac_designation: facDesigntion
+    };
+    alert("Faculty Added Successfully..!!");
+
+    console.log("Faculty Object :");
+    console.log(faculty);
+
+    facultyData.push(faculty);
+    console.log("Faculty in Array :");
+    console.log(facultyData);
+    // Storing in Local Storage
+    localStorage.setItem("point", JSON.stringify(facultyData));
+    
+    displayFaculty_In_Table();
+}
+
+
+displayFaculty_In_Table();
