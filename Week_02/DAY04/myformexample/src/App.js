1811002d0a859b929c2233a2271659025a8a609f
namrapithwa-tech <164359724+namrapithwa-tech@users.noreply.github.com>
2025-12-04 12:41:55 +0530

    React form Satrted

---
+++ Week_02/DAY04/myformexample/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FormExample from './components/FormExample/formexample';
+
+function App() {
+  return (
+    <>
+      <FormExample/>
+    </>
+  );
+}
+
+export default App;
